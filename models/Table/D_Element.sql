cb04535e92187b661357e14c38ab32639bb1ce60
nwawre2 <Niyati.Wawre@ofwat.gov.uk>
2021-04-30 11:21:40 +0000

    few changes

--- models/Table/D_Element.sql
+++ models/Table/D_Element.sql
@@ -9,7 +9,7 @@
     ,element_name
     ,element_acronym
     ,load_date
-    from {{ ref('D_Element_Inc')}}
+    from {{ ref('D_Element_inc')}}
     
 {% if is_incremental() %}
   where load_date >= (select max(load_date) from {{ this }})
