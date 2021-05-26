5f586fb24172204d08094a16c6bb69134cedf0a2
nwawre2 <Niyati.Wawre@ofwat.gov.uk>
2021-05-26 15:19:32 +0000

    txt file for snapshot changes and redirected code to pauls new spreadsheet

--- models/Table/D_Element.sql
+++ models/Table/D_Element.sql
@@ -1,17 +1,4 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='element_id'
-    )
-}}
-
-    select  {{dbt_utils.surrogate_key('element_acronym')}} element_id
+    select  {{dbt_utils.hash(dbt_utils.concat(['element_acronym','element_name']))}} element_id
     ,element_name
     ,element_acronym
-    ,load_date
-    from (select distinct element_name, element_acronym, load_date from {{ ref('PR14FinalCSVcreatedbyPythonView')}}) e
-
-
-{% if is_incremental() %}
-  where load_date >= (select max(load_date) from {{ this }})
-{% endif %}
+    from (select distinct element_name, element_acronym from {{ ref('PR14FinalCSVcreatedbyPythonView') }}) e
