a8bbfed9a77040fc6fe96e6adbf0fc7f526bcfcd
Niyati.Wawre <niyati.wawre@ofwat.gov.uk>
2021-06-23 14:27:56 +0000

    renaming column and notes and numeric

--- models/ViewStagingForPR14/stg_PR14_PC_company_pr.sql
+++ models/ViewStagingForPR14/stg_PR14_PC_company_pr.sql
@@ -27,11 +27,11 @@ final as (
     ,CAST(NULL as varchar(max)) as common_comparable_bespoke_performance_commitment
     ,annex
     ,direction_of_improving_performance
-    ,drinking_water_quality_compliance
-    ,water_quality_contacts
-    ,supply_interruptions_3_hours
-    ,pollution_incidents_cat_3
-    ,internal_sewer_flooding
+    ,PR14_comparative_drinking_water_compliance
+    ,PR14_comparative_water_quality_contacts
+    ,PR14_comparative_supply_interruptions_3_hours
+    ,PR14_comparative_pollution_incidents_cat_3
+    ,PR14_comparative_internal_sewer_flooding
     ,CAST(NULL as varchar(max)) as special_cost_factor
     ,scheme_specific_factor
     ,asset_health
@@ -42,42 +42,42 @@ final as (
     ,CAST(NULL as varchar(max)) as standard_odi_cal
     ,standard_odi_operand
     ,standard_odi_operand_note
-    ,[isnumeric_underp_payment1_incentive_rate_gbpm]
-    ,[isnumeric_underp_payment2_incentive_rate_gbpm]
-    ,[isnumeric_underp_payment3_incentive_rate_gbpm]
-    ,[isnumeric_underp_payment4_incentive_rate_gbpm]
-    ,[isnumeric_outp_payment1_incentive_rate_gbpm]
-    ,[isnumeric_outp_payment2_incentive_rate_gbpm]
-    ,[onlynumeric_underp_payment1_incentive_rate_gbpm]
-    ,[onlynumeric_underp_payment2_incentive_rate_gbpm]
-    ,[onlynumeric_underp_payment3_incentive_rate_gbpm]
-    ,[onlynumeric_underp_payment4_incentive_rate_gbpm]
-    ,[onlynumeric_outp_payment1_incentive_rate_gbpm]
-    ,[onlynumeric_outp_payment2_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as [isnumeric_underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [isnumeric_underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [isnumeric_underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [isnumeric_underp_payment_incentive_enhanced_underp_payment]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_underp_payment_incentive_enhanced_underp_payment]
-    ,CAST(NULL as varchar(max)) as [isnumeric_outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [isnumeric_outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [isnumeric_outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply]
-    ,CAST(NULL as varchar(max)) as [isnumeric_outp_payment_incentive_enhanced_outp_payment]
-    ,CAST(NULL as varchar(max)) as [onlynumeric_outp_payment_incentive_enhanced_outp_payment]
-    ,[price_control_allocation_water_resources]
-    ,[price_control_allocation_water_network_plus]
-    ,[price_control_allocation_wastewater_network_plus]
-    ,[price_control_allocation_bioresources_sludge]
-    ,[price_control_allocation_residential_retail]
-    ,[price_control_allocation_business_retail]
-    ,[price_control_allocation_direct_procurement_for_customers]
-    ,[price_control_allocation_dummy_control]
+    ,notes_underp_payment1_incentive_rate_gbpm
+    ,underp_payment1_incentive_rate_gbpm
+    ,notes_underp_payment2_incentive_rate_gbpm
+    ,underp_payment2_incentive_rate_gbpm
+    ,notes_underp_payment3_incentive_rate_gbpm
+    ,underp_payment3_incentive_rate_gbpm
+    ,notes_underp_payment4_incentive_rate_gbpm
+    ,underp_payment4_incentive_rate_gbpm
+    ,notes_outp_payment1_incentive_rate_gbpm
+    ,outp_payment1_incentive_rate_gbpm
+    ,notes_outp_payment2_incentive_rate_gbpm
+    ,outp_payment2_incentive_rate_gbpm
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment_incentive_enhanced_underp_payment]
+    ,CAST(NULL as varchar(max)) as [underp_payment_incentive_enhanced_underp_payment]
+    ,CAST(NULL as varchar(max)) as [notes_outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [notes_outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [notes_outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply]
+    ,CAST(NULL as varchar(max)) as [notes_outp_payment_incentive_enhanced_outp_payment]
+    ,CAST(NULL as varchar(max)) as [outp_payment_incentive_enhanced_outp_payment]
+    ,price_control_allocation_water_resources
+    ,price_control_allocation_water_network_plus
+    ,price_control_allocation_wastewater_network_plus
+    ,price_control_allocation_bioresources_sludge
+    ,price_control_allocation_residential_retail
+    ,price_control_allocation_business_retail
+    ,price_control_allocation_direct_procurement_for_customers
+    ,price_control_allocation_dummy_control
     from pr14 
         left join pc
         on isnull(ltrim(right(pr14.performance_commitment, len(pr14.performance_commitment) - charindex(':',pr14.performance_commitment))),'performance_commitment')=isnull(pc.performance_commitment,'performance_commitment')
--- models/ViewStagingForPR19/stg_PR19_PC_company_pr.sql
+++ models/ViewStagingForPR19/stg_PR19_PC_company_pr.sql
@@ -23,57 +23,57 @@ final as (
     ,common_and_comparable_bespoke_performance_commitment
     ,CAST(NULL as varchar(max)) as annex
     ,direction_of_improving_performance
-    ,CAST(NULL as varchar(max)) as drinking_water_quality_compliance
-    ,CAST(NULL as varchar(max)) as water_quality_contacts
-    ,CAST(NULL as varchar(max)) as supply_interruptions_3_hours
-    ,CAST(NULL as varchar(max)) as pollution_incidents_cat_3
-    ,CAST(NULL as varchar(max)) as internal_sewer_flooding
-    ,special_cost_factor
-    ,scheme_specific_factor
-    ,asset_health
-    ,nep
-    ,AIM
+    ,CAST(NULL as varchar(max)) as PR14_comparative_drinking_water_compliance
+    ,CAST(NULL as varchar(max)) as PR14_comparative_water_quality_contacts
+    ,CAST(NULL as varchar(max)) as PR14_comparative_supply_interruptions_3_hours
+    ,CAST(NULL as varchar(max)) as PR14_comparative_pollution_incidents_cat_3
+    ,CAST(NULL as varchar(max)) as PR14_comparative_internal_sewer_flooding
+    ,CAST(NULL as varchar(max)) as special_cost_factor
+    ,CAST(NULL as varchar(max)) as scheme_specific_factor
+    ,CAST(NULL as varchar(max)) as asset_health
+    ,CAST(NULL as varchar(max)) as nep
+    ,CAST(NULL as varchar(max)) as AIM
     ,customers_relative_priority
     ,CAST(NULL as varchar(max)) as no_of_sub_measures
     ,standard_odi_cal
     ,standard_odi_operand
     ,standard_odi_operand_note
-    ,CAST(NULL as varchar(max)) as [isnumeric_underp_payment1_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[isnumeric_underp_payment2_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[isnumeric_underp_payment3_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[isnumeric_underp_payment4_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[isnumeric_outp_payment1_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[isnumeric_outp_payment2_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[onlynumeric_underp_payment1_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[onlynumeric_underp_payment2_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[onlynumeric_underp_payment3_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[onlynumeric_underp_payment4_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[onlynumeric_outp_payment1_incentive_rate_gbpm]
-    ,CAST(NULL as varchar(max)) as[onlynumeric_outp_payment2_incentive_rate_gbpm]
-    ,[isnumeric_underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply]
-    ,[onlynumeric_underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply]
-    ,[isnumeric_underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply]
-    ,[onlynumeric_underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply]
-    ,[isnumeric_underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply]
-    ,[onlynumeric_underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply]
-    ,[isnumeric_underp_payment_incentive_enhanced_underp_payment]
-    ,[onlynumeric_underp_payment_incentive_enhanced_underp_payment]
-    ,[isnumeric_outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply]
-    ,[onlynumeric_outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply]
-    ,[isnumeric_outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply]
-    ,[onlynumeric_outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply]
-    ,[isnumeric_outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply]
-    ,[onlynumeric_outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply]
-    ,[isnumeric_outp_payment_incentive_enhanced_outp_payment]
-    ,[onlynumeric_outp_payment_incentive_enhanced_outp_payment]
-    ,[price_control_allocation_water_resources]
-    ,[price_control_allocation_water_network_plus]
-    ,[price_control_allocation_wastewater_network_plus]
-    ,[price_control_allocation_bioresources_sludge]
-    ,[price_control_allocation_residential_retail]
-    ,[price_control_allocation_business_retail]
-    ,[price_control_allocation_direct_procurement_for_customers]
-    ,[price_control_allocation_dummy_control]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment1_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [underp_payment1_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment2_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [underp_payment2_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment3_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [underp_payment3_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [notes_underp_payment4_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [underp_payment4_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [notes_outp_payment1_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [outp_payment1_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [notes_outp_payment2_incentive_rate_gbpm]
+    ,CAST(NULL as varchar(max)) as [outp_payment2_incentive_rate_gbpm]
+    ,notes_underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply
+    ,underp_payment_incentive_standard_underp_payment1_tier2_where_tiers_apply
+    ,notes_underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply
+    ,underp_payment_incentive_standard_underp_payment2_tier1_where_tiers_apply
+    ,notes_underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply
+    ,underp_payment_incentive_standard_underp_payment3_tier3_where_tiers_apply
+    ,notes_underp_payment_incentive_enhanced_underp_payment
+    ,underp_payment_incentive_enhanced_underp_payment
+    ,notes_outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply
+    ,outp_payment_incentive_standard_outp_payment1_tier2_where_tiers_apply
+    ,notes_outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply
+    ,outp_payment_incentive_standard_outp_payment2_tier1_where_tiers_apply
+    ,notes_outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply
+    ,outp_payment_incentive_standard_outp_payment3_tier3_where_tiers_apply
+    ,notes_outp_payment_incentive_enhanced_outp_payment
+    ,outp_payment_incentive_enhanced_outp_payment
+    ,price_control_allocation_water_resources
+    ,price_control_allocation_water_network_plus
+    ,price_control_allocation_wastewater_network_plus
+    ,price_control_allocation_bioresources_sludge
+    ,price_control_allocation_residential_retail
+    ,price_control_allocation_business_retail
+    ,price_control_allocation_direct_procurement_for_customers
+    ,price_control_allocation_dummy_control
     from pr19
         left join pc on isnull(pr19.performance_commitment,'performance_commitment') = isnull(pc.performance_commitment,'performance_commitment')
         and isnull(pr19.pc_unit,'pc_unit') = isnull(pc.pc_unit,'pc_unit')
